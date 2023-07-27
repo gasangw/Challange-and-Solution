6db13ee7139118757169b7ed6cc2f11263d90be7
gasangw <thomasgasangwa@gmail.com>
2023-07-27 18:42:00 +0200

    Give improved answers

--- Hangman/index.js
+++ Hangman/index.js
@@ -1,12 +1,21 @@
 function hangman(hangmanString) {
-    let str = hangmanString.split('')
-   let newStr = '';
-   for(let i = 0; i < str.length; i++){
-     if(i%2 == 0){
-       newStr+='_'
-     } else {
-       newStr+=str[i]
-     }
-   }
-   return `${newStr}`
+
+    // First Solution 
+
+
+//     let str = hangmanString.split('')
+//    let newStr = '';
+//    for(let i = 0; i < str.length; i++){
+//      if(i%2 == 0){
+//        newStr+='_'
+//      } else {
+//        newStr+=str[i]
+//      }
+//    }
+//    return `${newStr}`
+
+
+// Second and Improved solution
+
+return hangmanString.split('').map((ele, index)=> index%2 == 0? '_': ele).join('');
  }
