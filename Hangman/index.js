26b73ef3a9f2e905d9ff1ba1c008e5129503ba3e
gasangw <thomasgasangwa@gmail.com>
2023-07-27 18:37:31 +0200

    Add question to the challenge and the solution

--- Hangman/index.js
+++ Hangman/index.js
@@ -1,14 +1,12 @@
-// Given is a string hangmanString. Replace every second letter with an underscor
-// and return the result as a string. The first character must be an underscore.
-
-// Tests
-
-// hangman('TestString') '_e_t_t_i_g'
-
-// hangman('Foobar') '_o_b_r'
-
-// hangman('Demo') '_e_o'
-
-// hangman('JS is fancy') '_S_i_ _a_c_'
-
-// hangman('ThisIsAnFancyString') '_h_s_s_n_a_c_S_r_n_'
+function hangman(hangmanString) {
+    let str = hangmanString.split('')
+   let newStr = '';
+   for(let i = 0; i < str.length; i++){
+     if(i%2 == 0){
+       newStr+='_'
+     } else {
+       newStr+=str[i]
+     }
+   }
+   return `${newStr}`
+ }
