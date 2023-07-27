e599f0858b2e628622a48bc428086c551017251b
gasangw <thomasgasangwa@gmail.com>
2023-07-27 18:35:37 +0200

    Solvr the challenge of hangman

---
+++ Hangman/index.js
@@ -0,0 +1,14 @@
+// Given is a string hangmanString. Replace every second letter with an underscor
+// and return the result as a string. The first character must be an underscore.
+
+// Tests
+
+// hangman('TestString') '_e_t_t_i_g'
+
+// hangman('Foobar') '_o_b_r'
+
+// hangman('Demo') '_e_o'
+
+// hangman('JS is fancy') '_S_i_ _a_c_'
+
+// hangman('ThisIsAnFancyString') '_h_s_s_n_a_c_S_r_n_'
