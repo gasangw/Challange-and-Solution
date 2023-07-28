d1a5cf0c569cfecfc491d4aad66a7c2e6d282120
gasangw <thomasgasangwa@gmail.com>
2023-07-28 15:22:23 +0200

    Solve the challenge of max sum of sub-array

---
+++ Maximum-subarray-sum/index.js
@@ -0,0 +1,18 @@
+var maxSequence = function(arr){
+    // ...
+    if(arr.length == 0) return 0;
+    let currentSum = arr[0];
+    let globalSum = arr[0];
+    
+    for(let i = 1; i < arr.length; i++){
+      currentSum = Math.max(arr[i], currentSum + arr[i])
+      if(currentSum > globalSum){
+        globalSum = currentSum
+      }
+    } 
+    if(globalSum < 0) {
+      return 0
+    } else {
+      return globalSum;
+    }
+}
