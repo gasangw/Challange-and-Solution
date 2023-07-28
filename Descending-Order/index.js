ddec6bea1c850f4a8b678982ef3c492df803054e
gasangw <thomasgasangwa@gmail.com>
2023-07-28 11:22:26 +0200

    Add an improved solution

--- Descending-Order/index.js
+++ Descending-Order/index.js
@@ -1,4 +1,8 @@
 function descendingOrder(n){
     let num = n.toString().split('')
     return num.length <= 1? Number(num.join('')) : Number(num.sort((a,b) => Number(b) - Number(a)).join('')) 
+
+    // wonderful solution
+
+    // return parseInt(String(n).split('').sort().reverse().join(''))
   }
