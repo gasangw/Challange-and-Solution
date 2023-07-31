68f0f004f9a0b9883c3ab3103beb6bde26fe4a2a
gasangw <thomasgasangwa@gmail.com>
2023-07-31 10:47:44 +0200

    Add solution for maxMultiple

--- maxMultiple/index.js
+++ maxMultiple/index.js
@@ -1,16 +1,17 @@
-// Task
-// Given a Divisor and a Bound , Find the largest integer N , Such That ,
 
-// Conditions :
-// N is divisible by divisor
-
-// N is less than or equal to bound
-
-// N is greater than 0.
-
-
-//       maxMultiple(3,10),9);
-//       maxMultiple(7,17),14);
-//       maxMultiple(10,50),50);
-//       maxMultiple(37,200),185);
-//       maxMultiple(7,100),98);
+function maxMultiple(divisor, bound){
+    //your code here
+    
+    // declear an array that holds the occuring number
+    // loop through from zero to less or equal to bound,
+    // if i is divisible by divisor push to an array.
+    // sort the array and return the largest number
+    
+    let arr = [];
+    for(let i = 0; i <= bound; i++){
+      if(i % divisor == 0){
+        arr.push(i)
+      }
+    }
+    return Number(arr.sort((a,b)=> a-b).slice(-1).join(''))
+  }
