629321eef7ce71db789b8ec59c391c12fd022598
gasangw <thomasgasangwa@gmail.com>
2023-08-16 11:43:22 +0200

    Solve the challenge using Typescript

---
+++ Jaden Casing Strings/index.js
@@ -0,0 +1,13 @@
+// NOTE THE BELOW CHALLENGE WAS DONE IN TypeScript
+
+// String.prototype.toJadenCase = function () {
+//     return this.split(' ').map((word) => {
+//        return `${word[0].toUpperCase()}${word.slice(1)}`
+//     }).join(' ')
+//  }
+ 
+ 
+ 
+//  interface String {      // Declaration needed, don't remove it
+//    toJadenCase(): string;
+//  }
