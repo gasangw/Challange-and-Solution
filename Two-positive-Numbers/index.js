cea5039c8b7e86056ed09a5cc0800fa2c07f1a28
gasangw <thomasgasangwa@gmail.com>
2023-08-16 11:05:39 +0200

    Add improved solution

--- Two-positive-Numbers/index.js
+++ Two-positive-Numbers/index.js
@@ -2,11 +2,22 @@
 
 function twoArePositive(a, b, c) {
     // Happy Coding
-      if(a > 0 && b > 0 && c <= 0 || a > 0 && c > 0  && b <= 0 || b > 0 && c > 0 && a <= 0 || c > 0 && a > 0 && b<= 0){
-      return true;
-    } else {
-      return false;
-    }
+    //   if(a > 0 && b > 0 && c <= 0 || a > 0 && c > 0  && b <= 0 || b > 0 && c > 0 && a <= 0 || c > 0 && a > 0 && b<= 0){
+    //   return true;
+    // } else {
+    //   return false;
+    // }
+
+    // IMPROVED ANSWER
+    return [a,b,c].filter(num => num > 0).length == 2
   }
 
 // TypeScript Solution
+
+// export function twoArePositive(a: number, b: number, c: number): boolean {
+//     if(a > 0 && b > 0 && c <= 0 || a > 0 && c > 0  && b <= 0 || b > 0 && c > 0 && a <= 0 || c > 0 && a > 0 && b<= 0){
+//     return true;
+//   } else {
+//     return false;
+//   }
+// }
