458db1004e231c64e6b0a6e66f3a938b24b7f540
gasangw <thomasgasangwa@gmail.com>
2023-08-18 11:32:55 +0200

    Add more improved and cleaner solution

--- Tail Swap/index.ts
+++ Tail Swap/index.ts
@@ -1,13 +1,19 @@
-export function tailSwap(arr: [string,string]): [string,string] {
-    // your code here
-    let twoArr = arr.map((ele) => (ele.split(':')))
-    let firstArr = twoArr[0]
-    let secondArr = twoArr[1]
-     let fLast = firstArr.pop();
-     let sLast = secondArr.pop();
+// export function tailSwap(arr: [string,string]): [string,string] {
+//     // your code here
+//     let twoArr = arr.map((ele) => (ele.split(':')))
+//     let firstArr = twoArr[0]
+//     let secondArr = twoArr[1]
+//      let fLast = firstArr.pop();
+//      let sLast = secondArr.pop();
     
-     let NewFirstArr = [...firstArr, sLast]
-     let NewSecondArr = [...secondArr, fLast]
+//      let NewFirstArr = [...firstArr, sLast]
+//      let NewSecondArr = [...secondArr, fLast]
       
-    return [NewFirstArr.join(':')+'', NewSecondArr.join(':')+'']
+//     return [NewFirstArr.join(':')+'', NewSecondArr.join(':')+'']
+//   }
+
+  // Improved And Clearner Code
+  export function tailSwap(arr: [string,string]): [string,string] {
+    const [[a,b], [c, d]] = arr.map(ele => ele.split(":"))
+    return [`${a}:${d}`, `${c}:${b}`]
   }
