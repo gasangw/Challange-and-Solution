4419fb7e1235b7cddfe98c5dc2893c179b198cd2
gasangw <thomasgasangwa@gmail.com>
2023-08-22 11:14:15 +0200

    Solve the replace first value challenge

---
+++ Replace first/index.ts
@@ -0,0 +1,9 @@
+function replaceFirst(values: number[]): number[] {
+    // your code here
+    if(values.length <= 1) return values;
+    let first = values.shift();
+    return [...values, first]
+}
+
+console.log("Example:");
+console.log(JSON.stringify(replaceFirst([1, 2, 3, 4])));
