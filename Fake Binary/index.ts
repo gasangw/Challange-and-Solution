7820d3cd0e62a71344c2196346a78c14b8bccc4a
gasangw <thomasgasangwa@gmail.com>
2023-09-29 11:09:12 +0200

    Add the best solution

--- Fake Binary/index.ts
+++ Fake Binary/index.ts
@@ -1,3 +1,6 @@
 export const fakeBin = (x:string):string => {
     return  x.replace(/[01234]/g, '0').replace(/[56789]/g, '1')  
 };
+
+// BEST SOLUTION
+// return x.replace(/\d/g, n=> Number(n) < 5 ? '0': '1')
